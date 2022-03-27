678a4fc1e63402cff64f2336b9b9be09c5f80620
ely cheikh SMAIL <39855707+elycheikhsmail@users.noreply.github.com>
2022-03-27 02:05:17 +0000

    Update child.py

--- child.py
+++ child.py
@@ -2,12 +2,12 @@ import kajiki
 # "." mean files in current directory and his subdirectories
 loader = kajiki.FileLoader(".")
 
-c1 = loader.import_("c1.html")
+c1 = loader.import_("child.html")
 c1_str = c1().render()
 print(c1_str)
 
 
-p1 = loader.import_("p1.html")
+p1 = loader.import_("parent.html")
 
 print(p1(dict(content=c1_str)).render())
 
